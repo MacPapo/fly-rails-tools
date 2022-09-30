945fb4e7917deff483ae935e728475d75d8a95da
MacPapo <jacopocostantini32@gmail.com>
2022-10-01 00:53:04 +0200

    Refactoring...

--- fly-rails-tools.el
+++ fly-rails-tools.el
@@ -8,29 +8,28 @@
 (require 'projectile-rails)
 
 (defun fly-rails-open-on-safari ()
-  "Power on server and open Safari."
+  "Power on Rails Server in RoR project and then open Safari."
   (interactive)
   (let ((url "http://localhost:3000"))
     (fly-open-rails-server)
     (browse-url-default-macosx-browser url)))
 
-(defun fly-rails-close-server ()
-  "Delete buffer and also the window."
-  (interactive)
-  (if (get-buffer projectile-rails-server-buffer-name)
-      (save-current-buffer
-        (delete-windows-on
-         projectile-rails-server-buffer-name)
-        (let ((kill-buffer-query-functions nil))
-          (kill-buffer
-           projectile-rails-server-buffer-name)))
-    (message "No Buffer to Kill!!")))
-
 (defun fly-open-rails-server ()
-  "Start project server."
+  "Start Rails project Server."
   (unless (bufferp
            (get-buffer projectile-rails-server-buffer-name))
     (projectile-rails-server)))
+
+(defun fly-rails-close-server ()
+  "Delete Rails Server buffer and also the Rails Server buffer window."
+  (interactive)
+  (if (get-buffer projectile-rails-server-buffer-name)
+      (save-current-buffer
+        (delete-windows-on projectile-rails-server-buffer-name)
+        (let ((kill-buffer-query-functions nil))
+          (kill-buffer projectile-rails-server-buffer-name)
+          (message "Rails server and Rails server buffer closed!!")))
+    (message "No Buffer to Kill!!")))
 
 (provide 'fly-rails-tools)
 
